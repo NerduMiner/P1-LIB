797a41b8b0aa6ad81be3bba081834941ed0de151
intns <85960679+aioms@users.noreply.github.com>
2021-06-23 18:57:35 +0100

    Fill LFinfo

--- Includes/PKHeader/LFInfo.h
+++ Includes/PKHeader/LFInfo.h
@@ -2,15 +2,22 @@
 #define _LFINFO_H
 
 #pragma once
+#include <PKHeader/Colour.h>
+#include <PKHeader/Vector2f.h>
+#include <PKHeader/Vector3f.h>
 #include <types.hpp>
 
-class DllImport LFInfo {
+class DllImport LFInfo : public Colour {
 public:
     LFInfo(class LFInfo const&);
     LFInfo(void);
     class LFInfo& operator=(class LFInfo const&);
 
-    char _00[0x2C];
+    Vector3f m_vector4;  // _04
+    Vector2f m_vector10; // _10
+    Vector2f m_vector18; // _18
+    Vector2f m_vector20; // _28
+    u32 m_dword28;       // _28
 };
 
 #endif
